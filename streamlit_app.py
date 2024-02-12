36e36eb97f2e2e9b3bebfb3e8b6a145b07a1cb79
Martin Hocheol Shin <MartinShin@users.noreply.github.com>
2024-02-12 18:02:40 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@
 import altair as alt
 import pandas as pd
 import streamlit as st
+from bs4 import BeautifulSoup 
+
 
 """
 # 저는 streamlit을 모릅니다.
@@ -10,7 +12,6 @@ import streamlit as st
 """
 st.write("Hello World")
 
-from bs4 import BeautifulSoup 
 
 
 
